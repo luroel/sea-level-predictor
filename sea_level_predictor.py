a02a0395aeb88e0461b9aacac8f7f8d249868097
luroel <jl.rodrigueze@hotmail.com>
2025-06-18 12:30:53 -0600

    Leyendo datos

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,10 +4,10 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
-
+    df = pd.read_csv("epa-sea-level.csv").head()
+    print(df)
     # Create scatter plot
-
+    
 
     # Create first line of best fit
 
@@ -21,3 +21,5 @@ def draw_plot():
     # Save plot and return data for testing 
     plt.savefig('sea_level_plot.png')
     return plt.gca()
+
+draw_plot()
