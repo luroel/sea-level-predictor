47b10b19196d97ffea9bfd31ef2f851900fcb44e
luroel <jl.rodrigueze@hotmail.com>
2025-06-18 16:26:23 -0600

    Agregando nueva linea del 2000-2050

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -9,22 +9,29 @@ def draw_plot():
     x = df['Year']
     y = df['CSIRO Adjusted Sea Level']
     
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots() # Verify doc
     plt.scatter(x, y, color='blue', label='Data points')
 
     # Create first line of best fit 
     lin_reg = linregress(x,y) # Perform linear regression
     print(lin_reg)
     x_ax = pd.Series([i for i in range(1880,2051)])
-    y_ec = lin_reg.slope*x_ax + lin_reg.intercept
-    plt.plot(x_ax, y_ec, color='purple')
+    y_fit1 = lin_reg.slope*x_ax + lin_reg.intercept
+    plt.plot(x_ax, y_fit1, color='purple')
     
-
     # Create second line of best fit
-
-
+    new_ax = df.loc[df['Year'] >= 2000]
+    X = new_ax["Year"]
+    Y = new_ax["CSIRO Adjusted Sea Level"]
+    lin_reg2 = linregress(X,Y)
+    x_ax2 = pd.Series([i for i in range(2000,2051)])
+    Y_fit2 = lin_reg2.slope*x_ax2 + lin_reg2.intercept
+    plt.plot(x_ax2, Y_fit2, color='green')
+    
     # Add labels and title
-
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Sea Level - Inches")
+    ax.set_title("Rise in Sea Level")
     
     # Save plot and return data for testing 
     plt.savefig('sea_level_plot.png')
