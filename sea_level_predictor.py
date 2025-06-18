be025b7a570d56ba093b1f85f61f77009cbdc5c2
luroel <jl.rodrigueze@hotmail.com>
2025-06-18 15:05:15 -0600

    Dibujando en grafica de dispersión (x,y), la linea que mejor se ajusta entre el año 1880-2050 utilizando regresion lineal

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -5,12 +5,20 @@ from scipy.stats import linregress
 def draw_plot():
     # Read data from file
     df = pd.read_csv("epa-sea-level.csv").head()
-    print(df)
     # Create scatter plot
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
     
+    fig, ax = plt.subplots()
+    plt.scatter(x, y, color='blue', label='Data points')
 
-    # Create first line of best fit
-
+    # Create first line of best fit 
+    lin_reg = linregress(x,y) # Perform linear regression
+    print(lin_reg)
+    x_ax = pd.Series([i for i in range(1880,2051)])
+    y_ec = lin_reg.slope*x_ax + lin_reg.intercept
+    plt.plot(x_ax, y_ec, color='purple')
+    
 
     # Create second line of best fit
 
